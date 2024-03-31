9a53ae2830ee0f640b9c3b0e6ea2ccc2005f2861
Lucky <luckysolanki902@gmail.com>
2024-03-31 15:20:54 +0530

    improved styling of event dialog and detail page

--- pages/events/[eventname]/index.js
+++ pages/events/[eventname]/index.js
@@ -10,7 +10,9 @@ const EventDetailsPage = () => {
   const router = useRouter();
   const { eventname } = router.query;
   const [eventDetails, setEventDetails] = useState(null);
-  const linkPath = `/form/${encodeURIComponent(eventname.toLowerCase().replace(/\s+/g, '-'))}`;
+  const [loading, setLoading] = useState(true);
+  const linkPath = `/form/${encodeURIComponent(eventname?.toLowerCase().replace(/\s+/g, '-'))}`;
+  console.log(eventname, linkPath)
   useEffect(() => {
     const fetchEventDetails = async () => {
       try {
@@ -24,6 +26,7 @@ const EventDetailsPage = () => {
       } catch (error) {
         console.error('Error during fetch', error);
       }
+      setLoading(false);
     };
 
     if (eventname) {
@@ -36,31 +39,40 @@ const EventDetailsPage = () => {
       <Head>
         <title>{eventname}</title>
       </Head>
-      <div className={styles.mainbg}>
-        <div className={styles.formContent} data-cursor-color='rgba(255,155,255,0.7)'>
-          <h2 className={styles.mainHeading} >About The Event</h2>
-          <h1 className={styles.formTitle} >{eventname?.toUpperCase()}</h1>
-          {eventDetails && (
-            <p className={styles.subHeading}>{eventDetails.about}</p>
+      <div className={styles.mainbg} >
+        <div className={styles.formContent} style={loading ? { display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '20rem' } : {}} data-cursor-color='rgba(255,155,255,0.7)'>
+          {loading && (
+            <div style={{fontSize:'1.2rem'}}>Loading...</div>
           )}
-          <h3 className={styles.mainHeading} >Guidelines</h3>
-          <div className={styles.guidelinesPaper}>
-            <div>
-              {eventDetails && eventDetails.guidelines.map((guideline, index) => (
-                <ul key={index}>
-                  <li style={{ listStyleType: 'none', display: 'flex', alignItems: 'flex-start', textAlign: 'left', marginRight: '1rem', color: 'rgba(255, 255, 255, 0.8)' }} className={styles.guidelines}>
-                    <CircleIcon className={styles.guidelineDisk} /> {guideline}
-                  </li>
-                </ul>
-              ))}
-            </div>
-          </div>
-          <button onClick={router.push(linkPath)} type='submit' className={styles.myButton}>
-            Register Now
-          </button>
+          {!loading && (
+            <>
+              <h2 className={styles.mainHeading} >About The Event</h2>
+              <h1 className={styles.formTitle} >{eventname?.toUpperCase()}</h1>
+              {eventDetails && (
+                <p className={styles.subHeading}>{eventDetails.about}</p>
+              )}
+              <h3 className={styles.mainHeading} >Guidelines</h3>
+              <div className={styles.guidelinesPaper}>
+                <div>
+                  {eventDetails && eventDetails.guidelines.map((guideline, index) => (
+                    <ul key={index}>
+                      <li style={{ listStyleType: 'none', display: 'flex', alignItems: 'flex-start', textAlign: 'left', marginRight: '1rem', color: 'rgba(255, 255, 255, 0.8)' }} className={styles.guidelines}>
+                        <CircleIcon className={styles.guidelineDisk} /> {guideline}
+                      </li>
+                    </ul>
+                  ))}
+                </div>
+              </div>
+              <Button variant='outlined' href={linkPath} sx={{maxWidth:"20rem", borderRadius:'1rem', margin:'auto', padding:'0.6rem 1.5rem', marginTop:"2rem", fontSize:'1.2rem'}}>
+                Register Now
+              </Button>
+
+            </>
+          )}
         </div>
       </div>
     </>
   );
 };
 export default EventDetailsPage;
+
